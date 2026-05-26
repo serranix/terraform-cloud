976a47e5bd547041ac449e140330c37a20a01465
serranix <nserrano.serrano@gmail.com>
2026-05-26 21:07:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "terra-test" {
-  name     = "rg-terra-test"
-  location = "uksouth"
-}
+#resource "azurerm_resource_group" "terra-test" {
+#  name     = "rg-terra-test"
+#  location = "uksouth"
+#}
