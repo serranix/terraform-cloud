b8f7d3cf3328fd9bdd47b95ce01ff4951d713e29
serranix <nserrano.serrano@gmail.com>
2026-05-26 11:29:49 +0100

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "1.15.4"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.107"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
